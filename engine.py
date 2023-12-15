c21d30a1f7b765cae111a1dc9cd7e3b792c12c2d
Vincent <vincent.chan@snowflake.com>
2023-12-14 21:56:52 -0800

    wip

--- engine.py
+++ engine.py
@@ -19,7 +19,7 @@ class vLLMEngine:
     def __init__(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("--host", type=str, default="0.0.0.0")
-        parser.add_argument("--port", type=int, default=8000)
+        parser.add_argument("--port", type=int, default=1234)
         parser = AsyncEngineArgs.add_cli_args(parser)
         args, _ = parser.parse_known_args()
         engine_args = AsyncEngineArgs.from_cli_args(args)
