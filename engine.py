e00e640c855779073d892743cfbb197422d46433
Vincent <vincent.chan@snowflake.com>
2023-12-14 16:18:35 -0800

    wip

--- engine.py
+++ engine.py
@@ -50,9 +50,8 @@ class vLLMEngine:
                 async for request_output in results_generator:
                     text_outputs = []
                     for output in request_output.outputs:
-                        text_outputs.append(output.text[len(full_output):])
-                        full_output += output.text
-                    ret = {"text": text_outputs}
+                        print(request_output.outputs)
+                    ret = {"text": ""}
                     yield (json.dumps(ret) + "\0\n")
 
             return stream_results()
