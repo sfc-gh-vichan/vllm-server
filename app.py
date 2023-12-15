179f0aae9c7878ba7db680307154438a0b8f5a17
Vincent <vincent.chan@snowflake.com>
2023-12-14 16:06:57 -0800

    wip

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ async def health() -> Response:
 
 @app.post("/v1/generate")
 async def _generate(req: InferenceRequest) -> Response:
-    if req.stream:
-        return StreamingResponse(engine.generate(req))
     resp = await engine.generate(req)
+    if req.stream:
+        return StreamingResponse(resp)
     return JSONResponse(resp)
