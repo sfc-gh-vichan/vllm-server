fe0ef3d70dd6f990ab2ff0f50a1633b16d5f6cc7
Vincent <vincent.chan@snowflake.com>
2023-12-14 20:28:09 -0800

    wip

--- engine.py
+++ engine.py
@@ -83,5 +83,5 @@ class vLLMEngine:
 
             for request_output in request_outputs:
                 if request_output.finished:
-                    outputs.append(request_output.outputs.text)
+                    outputs.append(request_output.outputs[0].text)
         return {"text": outputs}
